be1cf09b63636acfcc8ce19e6d75f68f9498b708
byoungyup_kim <qudduq999@daum.net>
2024-10-06 23:25:46 +0900

    first commit

---
+++ src/components/Navibar.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+
+const Navibar = () => {
+  return (
+    <header className="fixed w-full">
+      <ul className="flex justify-around items-center h-[80px] bg-blue-500">
+        <Link href={"/"} className="text-white">
+          <li>홈</li>
+        </Link>
+        <Link href={"/champions"} className="text-white">
+          <li>챔피언 목록</li>
+        </Link>
+        <Link href={"/items"} className="text-white">
+          <li>아이템 목록</li>
+        </Link>
+        <Link href={"/rotation"} className="text-white">
+          <li>로테이션 목록</li>
+        </Link>
+      </ul>
+    </header>
+  );
+};
+
+export default Navibar;
